a6340e780b4ebf61c8037e081194bdab893eb4ed
Armend Azizi <armend.azizi@usi.ch>
2018-02-08 16:54:34 +0100

    Merge branch 'master' into branchTest

--- test.java
+++ test.java
@@ -3,9 +3,11 @@ public class test{
     public static void main(String[]args){
 
     System.out.println("This is java fellows");
-    
-    System.out.println("Ok, but I want to write something else from GITHUB");
-        
+
+    System.out.println("Ok, but I want to write something else from GITHUB\n");
+
+
+    System.out.println("Hold on guys, branchtest is here");
 
     }
 
