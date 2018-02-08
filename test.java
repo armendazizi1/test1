b8615963cbb161175fc915fac3f9fc744fd586f3
armendazizi1 <armend.azizi@usi.ch>
2018-02-08 16:52:06 +0100

    Update test.java

--- test.java
+++ test.java
@@ -3,6 +3,9 @@ public class test{
     public static void main(String[]args){
 
     System.out.println("This is java fellows");
+    
+    System.out.println("Ok, but I want to write something else from GITHUB");
+        
 
     }
 
