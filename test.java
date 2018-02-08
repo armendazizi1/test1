5a808dc1a772c2fbcea8d7f9cab50331e1155f8b
Armend Azizi <armend.azizi@usi.ch>
2018-02-08 16:57:02 +0100

    Updated from master

--- test.java
+++ test.java
@@ -3,9 +3,12 @@ public class test{
     public static void main(String[]args){
 
     System.out.println("This is java fellows");
-    
+
     System.out.println("Ok, but I want to write something else from GITHUB");
-        
+
+
+      System.out.println("This is just the \"Master branch\"");
+
 
     }
 
